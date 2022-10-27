2708514a6579fbad8808281e4b1ee2a8b5ad7af1
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-10-27 18:53:19 -0500

    Primera subida de archivo de Python

---
+++ Nueva carpeta/hola_muundo.py
@@ -0,0 +1,10 @@
+# EJEMPLO 1.-Saludo
+# COMENTARIOS: Sirven para documentar o detallar cada parte del programa
+# Los comentarios no los toma en cuenta Python
+
+# Imprimiento o mostrando en pantalla mensajes o resultados
+
+print("hola mundo")
+print('Eduardo');
+print(10+5)
+print(9+8)
