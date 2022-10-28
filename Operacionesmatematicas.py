5df22a914dbcd52859bc0e41ee569832cbfc19c7
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-10-27 21:23:34 -0500

    Operacionesmatematicas

---
+++ Operacionesmatematicas.py
@@ -0,0 +1,37 @@
+# Ejemplo 2. Operaciones Matematicas
+
+# ENTRADAAS DE DATOS
+# Declarar o ceRrar las variables
+numero_1 = 10
+numero_2 = 2
+# Declarar o crear constatntes: Elemento que su valor permanece fijo
+PI = 3.1516
+
+
+# procesos (Cálculos u Operaciones Matemáticas Y/Lógicas if, elif y else)
+suma = numero_1 + numero_2
+resta = numero_1 - numero_2
+multiplicación = numero_1 * numero_2
+división = numero_1 / numero_2
+
+
+
+# SALIDAS DE DATOS (Mostrarlos resultados)
+print("La suma es = ", suma)
+print("La suma es = " + str (suma)) #CONCATENACION: Unión de dos o más textos
+# Conversión d eun tipo de dato en otro tipo de dato (CASTEO)
+print (f"La suma es = {suma}") # Interpolación de textos f : Formateo
+
+
+print("La resta es =", resta)
+print("La resta es =" + str (resta))
+print(f"La resta es = {resta}")
+
+
+print("La multiplicación es =", multiplicación)
+print("La multiplicación es = " + str (multiplicación))
+print(f"La multiplicación es = {multiplicación}")
+
+print("La división es =", división)
+print("La división es =" + str (división))
+print(f"La división es = {división}")
