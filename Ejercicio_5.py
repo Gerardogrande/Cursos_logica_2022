c48f65f28273e28c42de514f561e018cd35b53ca
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-03 18:44:55 -0600

    Add files via upload

--- Ejercicio_5.py
+++ Ejercicio_5.py
@@ -12,10 +12,12 @@ c = float(input("Ingresa el valor de c: "))
 
 
 # PROCESO
-x_1 = (-b + ((b**2 - (4*a*c))**1/2)) / (2*a)
-x_2 = (-b - ((b**2 - (4*a*c))**1/2)) / (2*a)
+x_1 = (-b + ((b**2 - (4*a*c))**(1/2))) / (2*a)
+x_2 = (-b - ((b**2 - (4*a*c))**(1/2))) / (2*a)
 
-
+# OPCIÓN 2
+x_1 = (-b + pow((b**2 -(4*a*c)), (1/2))) / (2*a)
+x_2 = (-b - pow((b**2 -(4*a*c)), (1/2))) / (2*a)
 
 # SALIDA DE DATOS
 print("x_1 es =", round(x_1, 1))
@@ -23,5 +25,5 @@ print("x_1 es = " + str(round(x_1, 1)))
 print(f"x_1 es = {round(x_1, 1)}")
 
 print("x_2 es =", round(x_2, 1))
-print("x_1 es = " + str(round(x_2, 1)))
-print(f"x_1 es = {round(x_2, 1)}")
+print("x_2 es = " + str(round(x_2, 1)))
+print(f"x_2 es = {round(x_2, 1)}")
