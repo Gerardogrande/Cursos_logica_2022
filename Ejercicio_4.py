c694c1e7050cd7f892ab69c890d8094de5508215
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-03 19:38:41 -0600

    Add files via upload

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -7,22 +7,38 @@ grados = float(input("Escribe el número de grados: "))
 
 
 # PROCESO
-celsius = grados - 273.15
-fahrenheit = ((9 * grados) / 5) + 32
-kelvin = grados + 273.15
+
+celsius_1 = grados - 273.5
+celsius_2 = ( 5* (grados - 32)) / 9
+kelvin_1 = grados + 273.15
+kelvin_2 = ((5 * (grados - 32)) / 9) + 273.15
+fahrenheit_1 = ((9 * grados) / 5) + 32
+fahrenheit_2 = ((9 * (grados - 273.15)) / 5) + 32
+
 
 
 
 # SALIDA DE DATOS
-print("Grados fahrenheit es =", round(fahrenheit, 2))
-print("Grados faherenheit es = " + str(round(fahrenheit, 2 )))
-print(f"grados faherenheit es = {round(fahrenheit, 2)}")
+print("Grados celcius_1 es =", round(celsius_1, 2))
+print("Grados celcius_1 es = " + str (round(celsius_1, 2)))
+print(f"Grados celcius_1 es = {round(celsius_1, 2)}")
 
+print("Grados celcius_2 es =", round(celsius_2, 2))
+print("Grados celsius_2 es =" + str(round(celsius_2, 2)))
+print(f"Grados celsius_2 es = {round(celsius_2, 2)}")
 
-print("Grados kelvin =", round(kelvin, 2))
-print("Grados kelvin = " + str(round(kelvin, 2)))
-print(f"Grados kelvin = {round(kelvin, 2)}")
+print("Grados kelvin__1 es =", round(kelvin_1, 2))
+print("Grados kelvin_1 es = " + str(round(kelvin_1, 2)))
+print(f"Grados kelvin_1 es = {round(kelvin_1, 2)}")
 
-print("Grados celcius =", round(celsius, 2))
-print("Grados celcius =" + str (round(celsius, 2)))
-print(f"Grados celcius = {round(celsius, 2)}")
+print("Grados kelvin_2 es =", round(kelvin_2, 2))
+print("Grados kelvin_2 es = " + str(round(kelvin_2, 2)))
+print(f"Grados kelvin_2 es = {round(kelvin_2, 2)}")
+
+print("Grados fahrenheit_1 es =", round(fahrenheit_1, 2))
+print("Grados faherenheit_1 es = " + str(round(fahrenheit_1, 2)))
+print(f"grados faherenheit_1 es = {round(fahrenheit_1, 2)}")
+
+print("Grados fahrenheit_2 es =", round(fahrenheit_2, 2))
+print("Grados fahrenheit_2 es = " + str(round(fahrenheit_2, 2)))
+print(f"Grados fahrenheit_2 es = {round(fahrenheit_2, 2)}")
