14f4b2ccf5a5e1937b34693979d07989e27a3f47
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-03 18:57:12 -0600

    Add files via upload

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -7,6 +7,7 @@ grados = float(input("Escribe el número de grados: "))
 
 
 # PROCESO
+celsius = grados - 273.15
 fahrenheit = ((9 * grados) / 5) + 32
 kelvin = grados + 273.15
 
@@ -15,9 +16,13 @@ kelvin = grados + 273.15
 # SALIDA DE DATOS
 print("Grados fahrenheit es =", round(fahrenheit, 2))
 print("Grados faherenheit es = " + str(round(fahrenheit, 2 )))
-print(f"grados faherenheit = {round(fahrenheit, 2)}")
+print(f"grados faherenheit es = {round(fahrenheit, 2)}")
 
 
 print("Grados kelvin =", round(kelvin, 2))
 print("Grados kelvin = " + str(round(kelvin, 2)))
 print(f"Grados kelvin = {round(kelvin, 2)}")
+
+print("Grados celcius =", round(celsius, 2))
+print("Grados celcius =" + str (round(celsius, 2)))
+print(f"Grados celcius = {round(celsius, 2)}")
