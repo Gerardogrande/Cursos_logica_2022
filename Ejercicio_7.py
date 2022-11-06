235fbee6b6a869c8b6517846f4c30980ef323f1a
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-05 23:32:38 -0600

    Add files via upload

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -3,13 +3,12 @@
 
 nivel_del_agua = float(input("Escribe el nivel del agua en metros: "))
 
-
 if(nivel_del_agua < 0):                             # menor a 0 m fuga en sisterna
     print("Fuga en Sisterna")
 elif(nivel_del_agua == 0):                          # nivel de 0 m Encender bomba de agua
     print("Encender Bomba de Agua")
 elif(nivel_del_agua > 0 and nivel_del_agua < 2):    # Entre 0 m y 2 m Acelerar bomba de agua
-    print("Acelerar Bombda Agua")
+    print("Acelerar Bomba de Agua")
 elif(nivel_del_agua > 2 and nivel_del_agua < 4):    # Entre 2 m y 4 m Bomba trabajando
     print("Bomba Trabajando")
 elif(nivel_del_agua > 4 and nivel_del_agua < 6):    # Entre 4m y 6 m Desacelerar bomba
