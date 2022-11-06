ece69adf037cc295d02463410232b421528c9415
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-06 11:05:28 -0600

    Add files via upload

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -7,13 +7,13 @@ if(nivel_del_agua < 0):                             # menor a 0 m fuga en sister
     print("Fuga en Sisterna")
 elif(nivel_del_agua == 0):                          # nivel de 0 m Encender bomba de agua
     print("Encender Bomba de Agua")
-elif(nivel_del_agua > 0 and nivel_del_agua < 2):    # Entre 0 m y 2 m Acelerar bomba de agua
+elif(nivel_del_agua > 0 and nivel_del_agua <= 2):   # Entre 0 m y 2 m Acelerar bomba de agua
     print("Acelerar Bomba de Agua")
-elif(nivel_del_agua > 2 and nivel_del_agua < 4):    # Entre 2 m y 4 m Bomba trabajando
+elif(nivel_del_agua > 2 and nivel_del_agua <= 4):   # Entre 2 m y 4 m Bomba trabajando
     print("Bomba Trabajando")
 elif(nivel_del_agua > 4 and nivel_del_agua < 6):    # Entre 4m y 6 m Desacelerar bomba
     print("Desacelerar Bomba")
 elif(nivel_del_agua == 6):                          # Nivel de 6 m. Apagar bomba
     print("Apagar Bomba")
-elif(nivel_del_agua > 6):                           # Si llega más de 6 m. Desbordamiento de agua en cisterna
+else:                                               # Si llega más de 6 m. Desbordamiento de agua en cisterna
     print("Desbordamiento de Agua en Cisterna")
