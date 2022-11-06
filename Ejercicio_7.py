01a4a590a48d120901f374e0f0828b40d616a4b2
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-06 11:18:38 -0600

    Add files via upload

--- Ejercicio_7.py
+++ Ejercicio_7.py
@@ -3,17 +3,18 @@
 
 nivel_del_agua = float(input("Escribe el nivel del agua en metros: "))
 
-if(nivel_del_agua < 0):                             # menor a 0 m fuga en sisterna
+# SALIDA DE DATOS
+if(nivel_del_agua < 0):
     print("Fuga en Sisterna")
-elif(nivel_del_agua == 0):                          # nivel de 0 m Encender bomba de agua
+elif(nivel_del_agua == 0):
     print("Encender Bomba de Agua")
-elif(nivel_del_agua > 0 and nivel_del_agua <= 2):   # Entre 0 m y 2 m Acelerar bomba de agua
+elif(nivel_del_agua > 0 and nivel_del_agua <= 2):
     print("Acelerar Bomba de Agua")
-elif(nivel_del_agua > 2 and nivel_del_agua <= 4):   # Entre 2 m y 4 m Bomba trabajando
+elif(nivel_del_agua > 2 and nivel_del_agua <= 4):
     print("Bomba Trabajando")
-elif(nivel_del_agua > 4 and nivel_del_agua < 6):    # Entre 4m y 6 m Desacelerar bomba
+elif(nivel_del_agua > 4 and nivel_del_agua < 6):
     print("Desacelerar Bomba")
-elif(nivel_del_agua == 6):                          # Nivel de 6 m. Apagar bomba
+elif(nivel_del_agua == 6):
     print("Apagar Bomba")
-else:                                               # Si llega más de 6 m. Desbordamiento de agua en cisterna
+else:
     print("Desbordamiento de Agua en Cisterna")
