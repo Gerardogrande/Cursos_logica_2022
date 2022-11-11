fd80837bd189db57fd9dd28dccad07dd25a67b08
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-11 15:19:39 -0600

    Add files via upload

--- Ejercicio_8.py
+++ Ejercicio_8.py
@@ -3,14 +3,14 @@
 # b) si es positivo y menor a 100 mostrar los números pares de forma ASCENDENTE
 # c) en otro caso imprimir No Válido
 
-j = número = float(input("Ingresa un número: "))
+j = número = float(input("Ingresa un número entero mayor a -100 y menor que 0 o un número menor que 100 y mayor que 0: "))
 
 if (j < 0 and j > -100):
     for j in range(-1, -100, -2):
-         print(f"{j}")
+         print("Número impar negativo = " f"{j}")
 elif(j > 0 and j < 100):
     for j in range(2, 101, 2):
-        print(f"{j}")
-else:
-    print("Número Inválido")
+        print("Número par positivo = " f"{j}")
+elif(j <= -100 or j == 0 or j >= 100):
+    print("Número no válido")
     
