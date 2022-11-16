1c35276065061ad8da52d8d436f88de7baf6e55b
Gerardogrande <116772603+Gerardogrande@users.noreply.github.com>
2022-11-15 20:55:16 -0600

    Add files via upload

--- EVALUACIÓN.py
+++ EVALUACIÓN.py
@@ -1,73 +1,217 @@
+print()
 print("EVALUACIÓN FINAL")
 print("Ejercicios")
+print()
 
 print("1. Herramienta de programación, parecido al lenguaje español utilizado para crear código.")
-incisos_1 =    "a) IDE,     b) Pseudocódigo,     c) Compilador,     d) ANSI / ISO"
+incisos_1 =    input("a) IDE     b) Pseudocódigo     c) Compilador     d) ANSI / ISO")
 print(f"{incisos_1}")
 print()
 
-
 print("2. Conjunto de símbolos, letras, números, imágenes, audio y video organizadas y que son relevantes en un tiempo y forma determinados.")
-incisos_2 =     "a) Información     b) Datos     c) Programa     d) Código"
+incisos_2 =     input("a) Información     b) Datos     c) Programa     d) Código")
 print(f"{incisos_2}")
 print()
 
-
 print("3. Instituciones encargadas de estandarizar reglas y simbología de los Diagramas de Flujo.")
-incisos_3 =     "a) IEEE     b) IDE     c) ANSI/ISO     d) VSCode"
+incisos_3 =     input("a) IEEE     b) IDE     c) ANSI/ISO     d) VSCode")
 print(f"{incisos_3}")
 print()
 
-
 print("4. Serie de pasos consecutivos, ordenados y finitos que se siguen para resolver un problema.")
-incisos_4 =     "a) Proceso     b) Solución     c) Función     d) Algoritmo"
+incisos_4 =     input("a) Proceso     b) Solución     c) Función     d) Algoritmo")
 print(f"{incisos_4}")
 print()
 
-
 print("5. Conjunto de elementos que se relacionan para cumplir una función determinada.")
-incisos_5 =    "a) Estructura     b) Datos     c) Operación     d) Sistema"
+incisos_5 =    input("a) Estructura     b) Datos     c) Operación     d) Sistema")
 print(f"{incisos_5}")
 print()
 
-
 print("6. Componente de un IDE que se encarga de traducir el código fuente a código máquina.")
-incisos_6 =     "a) Depurador     b) Editor de Texto     c) Terminal de Salida     d) Compilador/Intérprete"
+incisos_6 =     input("a) Depurador     b) Editor de Texto     c) Terminal de Salida     d) Compilador/Intérprete")
 print(f"{incisos_6}")
 print()
 
-
 print("7. Elemento que se usa para almacenar una cantidad donde cambia su valor.")
-incisos_7 =     "a) Constante     b) Variable     c) Librería     d) Tipo de Dato"
+incisos_7 =     input("a) Constante     b) Variable     c) Librería     d) Tipo de Dato")
 print(f"{incisos_7}")
 print()
 
-
 print("8. Conjunto de símbolos, letras, números que no tienen un significado.")
-incisos_8 =     "a) Datos     b) Estructura     c) Información     d) Sistema"
+incisos_8 =     input("a) Datos     b) Estructura     c) Información     d) Sistema")
 print(f"{incisos_8}")
 print()
 
-
 print("9. Disciplina que argumenta conclusiones a partir de premisas mediante un razonamiento.")
-incisos_9 =      "a) Filosofía     b) Sociología     c) Lógica     d) Argumentación"
+incisos_9 =      input("a) Filosofía     b) Sociología     c) Lógica     d) Argumentación")
 print(f"{incisos_9}")
 print()
 
-
 print("10. Medida, patrón, modelo o norma universal para realizar una actividad o producir un objeto.")
-incisos_10 =     "a) Evento     b) Estándar     c) Calidad     d) Productividad"
+incisos_10 =     input("a) Evento     b) Estándar     c) Calidad     d) Productividad")
 print(f"{incisos_10}")
 print()
 
-
 print("11. Conjunto de elementos ordenados que componen y son la base de algo físico o no físico.")
-incisos_11 =      "a) Estructura     b) Sistema     c) Objeto     d) Virtual"
+incisos_11 =      input("a) Estructura     b) Sistema     c) Objeto     d) Virtual")
 print(f"{incisos_11}")
 print()
 
-
 print("12. Conjunto de instrucciones (código) que indican a la computadora tareas a realizar.")
-incisos_12 =      "a) Operaciones/Cálculos     b) Sintaxis     c) Programa de Computadora     d) Comando"
+incisos_12 =      input("a) Operaciones/Cálculos     b) Sintaxis     c) Programa de Computadora     d) Comando")
 print(f"{incisos_12}")
 print()
+
+opciones = ["a", "b", "c", "d"]
+
+respuesta1 = input("Escribe el inciso de tu respuesta a la pregunta 1: ")
+if(respuesta1 == opciones[1]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta2 = input("Escribe el inciso de tu respuesta a la pregunta 2: ")
+if(respuesta2 == opciones[0]):
+   print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta3 = input("Escribe el inciso de tu respuesta a la pregunta 3: ")
+if(respuesta3 == opciones[2]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta4 = input("Escribe el inciso de tu respuesta a la pregunta 4: ")
+if(respuesta4 == opciones[0]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta5 = input("Escribe el inciso de tu respuesta a la pregunta 5: ")
+if(respuesta5 == opciones[3]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta6 = input("Escribe el inciso de tu respuesta a la pregunta 6: ")
+if(respuesta6 == opciones[3]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta7 = input("Escribe el inciso de tu respuesta a la pregunta 7: ")
+if(respuesta7 == opciones[1]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta8 = input("Escribe el inciso de tu respuesta a la pregunta 8: ")
+if(respuesta8 == opciones[0]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta9 = input("Escribe el inciso de tu respuesta a la pregunta 9: ")
+if(respuesta9 == opciones[2]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta10 = input("Escribe el inciso de tu respuesta a la pregunta 10: ")
+if(respuesta10 == opciones[1]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta11 = input("Escribe el inciso de tu respuesta a la pregunta 11: ")
+if(respuesta11 == opciones[0]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+respuesta12 = input("Escribe el inciso de tu respuesta a la pregunta 12: ")
+if(respuesta12 == opciones[2]):
+    print("La respuesta es correcta")
+else:
+    print("La respuesta es incorrecta")
+print()
+
+aciertosC = 0
+
+if(respuesta1 == opciones[1]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta2 == opciones[0]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta3 == opciones[2]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta4 == opciones[0]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta5 == opciones[3]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta6 == opciones[3]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta7 == opciones[1]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta8 == opciones[0]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta9 == opciones[2]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta10 == opciones[1]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta11 == opciones[0]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+if(respuesta12 == opciones[2]):
+    aciertosC = aciertosC + 1
+else:
+    aciertosC
+
+print("Total de aciertos correctos =", aciertosC)
+
+promedio = (aciertosC * 10) / 12
+print("Tu promedio es de =", round(promedio, 1))
